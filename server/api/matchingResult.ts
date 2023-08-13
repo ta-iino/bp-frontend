4c1c492fc9d47ed2d2f7339e495f15ee683915d0
yu-oosako <yu-oosako@comcosystem.co.jp>
2023-08-13 22:51:53 +0900

    mockAPiのデータ修正

--- server/api/matchingResult.ts
+++ server/api/matchingResult.ts
@@ -4,9 +4,17 @@ export default defineEventHandler(() => {
     {
       "id": 1,
       "sendCompanyHistoryId": 2,
-      "buyneedsId": 41,
-      "candidateCompanyId": 22
-  },
+      "buyneeds_id": 41,
+      "candidateCompanyId": 22,
+      "valuationScore": 80
+    },
+    {
+      "id": 2,
+      "sendCompanyHistoryId": 3,
+      "buyneeds_id": 43,
+      "candidateCompanyId": 25,
+      "valuationScore": 70
+    },
   ];
 
   return data;

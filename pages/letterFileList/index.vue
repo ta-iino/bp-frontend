f571b4b1b71e525bf89dc00a5ea40acb3c2db3e6
ta-iino <ta-iino_b@comcosystem.co.jp>
2024-06-21 15:24:52 +0900

    レターファイル一覧画面モック作成

--- pages/letterFileList/index.vue
+++ pages/letterFileList/index.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
     <div id="app">
-      <!-- ヘッダ部分 ここから -->
       <v-container class="pb-0">
+        <!-- ヘッダ部分 ここから -->
         <v-row>
           <v-col cols="12" class="px-0 my-3 py-0" style="background-color: #81D4FA">
             <v-card-title class="headline font-weight-bold">
@@ -10,8 +10,63 @@
             </v-card-title>
           </v-col>
         </v-row>
+        <!-- ヘッダ部分 ここまで -->
+        <!-- 一覧表示部分 ここから -->
+      　<v-row>
+          <v-data-table
+            :headers="headers"
+            hide-default-footer
+            class="elevation-1 ui-vdatatable"
+            show-select
+          >
+            <!-- ページネーション ここから -->
+            <template #bottom>
+              <div class="text-center pt-2">
+                <v-pagination
+                  v-model="page"
+                  cols="12"
+                  :length="totalPage"
+                  :total-visible="15"
+                ></v-pagination>
+              </div>
+            </template>
+            <!-- ページネーション ここまで -->
+          </v-data-table>
+        </v-row>
+        <!-- 一覧表示 ここまで -->
+        <!-- 一覧表示部分 ここまで -->
+        <!-- ボタンエリア ここから -->
+        <v-row>
+          <v-col cols="9" />
+          <v-col cols="1">
+            <v-btn class="ui-btn bg-grey-lighten-2" width="100%" @click="$router.back()">
+              戻る
+            </v-btn>
+          </v-col>
+          <v-col cols="2">
+            <v-btn class="ui-btn bg-grey-lighten-2" width="100%">
+              ダウンロード
+            </v-btn>
+          </v-col>
+        </v-row>
+        <!-- ボタンエリア ここまで -->
       </v-container>
-      <!-- ヘッダ部分 ここまで -->
     </div>
   </v-app>
 </template>
+
+<script setup lang="ts">
+import { VDataTable } from 'vuetify/lib/labs/components.mjs'
+
+// ヘッダ
+const headers = ref(
+  [
+    { title: 'タイトル', key: 'title'　},
+    { title: 'DMリスト', key: 'dmList'　},
+    { title: 'レター作成日時', key: 'datetime'　},
+    { title: '依頼者', key: 'requester'　},
+    { title: 'ステータス', key: 'status'　}
+  ]
+)
+
+</script>
